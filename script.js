f96c20142cf73e0bc23a717b52ae27e69d7e44a5
Angellyn218 <angellyncervantes@yahoo.com>
2022-09-27 14:24:20 -0700

    Added new alerts to number of characters section of generate password to check for all cases

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -20,14 +19,18 @@ generateBtn.addEventListener("click", writePassword);
 // Generate password
 function generatePassword() {
   var num = prompt("Please the number of characters that you want (8-128 character required)");
-  if (num < 8) {
-    alert("Not enough characters. Try again");
+  if ((num >= 8) && (num <= 128)) {
+    alert("Great! You want a password with " + num + " characters.");
+  } else if (num < 8) {
+    alert("Not enough characters. Try again.");
     return;
   } else if (num > 128) {
-    alert("Too many characters. Try again");
+    alert("Too many characters. Try again.");
     return;
+  } else {
+    alert ("Not a number. Try again.")
   }
-  
+
   return;
 };
 
